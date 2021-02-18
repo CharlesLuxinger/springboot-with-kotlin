808b08bfaa71fd86be22158572225382fc4dc3aa
Charles Luxinger <charlesluxinger@gmail.com>
2021-02-18 18:30:24 -0300

    Awaytur Project

---
+++ awaytur/src/test/kotlin/com/github/charlesluxinger/awaytur/AwayturApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.charlesluxinger.awaytur
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AwayturApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
